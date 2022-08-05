e9db36d02f92965e38136e867bc98484fa25c2dc
Ahmed Dewedar <dr.a.dewedar@gmail.com>
2022-08-05 02:34:02 +0200

    Object Initialization

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LinkedIn.Essentials;
+
+Employee e1 = new Employee("Ahmed", "Dewedar");
+Console.WriteLine($"Welcome {e1.FirstName} {e1.LastName} as an Employer.");
+
+Employee e2 = new Employee()
+    {
+    FirstName = "Ahmed",
+    LastName = "Dewedar",
+    Id = 45
+    };
+
+Console.WriteLine($"{e2.FirstName} {e2.LastName} has Id {e2.Id}");
+
+
+Employee m = new Manager("Ahmed", "Dewedar");
+Console.WriteLine($"Welcome {m.FirstName} {m.LastName} as a Manager.");
