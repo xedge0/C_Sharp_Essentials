0b948e1e0411689e6a7c646b7db5f67b6efe439a
Ahmed Dewedar <dr.a.dewedar@gmail.com>
2022-08-06 19:25:55 +0200

    Guard conditions

--- Program.cs
+++ Program.cs
@@ -2,11 +2,38 @@
 using System.Diagnostics.CodeAnalysis;
 
 string? input = "something|";
+Console.WriteLine(PadAndTrim(input, 15, 'X'));
 
-Console.WriteLine(PadAndTrim(input, 15, '0'));
+static string PadAndTrim([AllowNull] string input, int length, char padChar)
+    {
+    if(input is null)
+        {
+        return String.Empty.PadLeft(length, padChar);
+        }
+
+    else if(input.Length <= length && input != null)
+        {
+        switch(padChar)
+            {
+            case (>= 'a' and <= 'z') or (>= 'A' and <= 'Z') when padChar != 'X':
+                return input.Trim().PadLeft(length, padChar);
+            case (>= 'a' and <= 'z') or (>= 'A' and <= 'Z') when padChar == 'X':
+                return String.Empty.PadLeft(length, '_');
+            case (>= '0' and <= '9'):
+                return input.Trim().PadRight(length, padChar);
+            default:
+                Console.WriteLine("No match found for pad character");
+                break;
+            }
+        return input.Trim().PadLeft(length, padChar);
+        }
+    else
+        {
+        throw new ArgumentException("Input is longer than the requested length");
+        }
+    }
 
 var shiftDay = GetShiftDays(DateTime.Now.DayOfWeek);
-//var shiftDay = GetShiftDays((DayOfWeek)17); // throw exception
 Console.WriteLine(shiftDay);
 
 static ShiftDays GetShiftDays(DayOfWeek day) => day switch
@@ -31,36 +58,8 @@ static bool IsWeekendNightShift(DateTime shiftDate) => shiftDate switch
         _ => false
         };
 
-static string PadAndTrim([AllowNull] string input, int length, char padChar)
-    {
-    if(input is null)
-        {
-        return String.Empty.PadLeft(length, padChar);
-        }
-
-    else if(input.Length <= length && input != null)
-        {
-        switch(padChar)
-            {
-            case (>= 'a' and <= 'z') or (>= 'A' and <= 'Z'):
-                return input.Trim().PadLeft(length, padChar);
-            case (>= '0' and <= '9'):
-                return input.Trim().PadRight(length, padChar);
-            default:
-                Console.WriteLine("No match found for pad character");
-                break;
-            }
-        return input.Trim().PadLeft(length, padChar);
-        }
-    else
-        {
-        throw new ArgumentException("Input is longer than the requested length");
-        }
-    }
-
 IPerson sw = new ShiftWorker() { FirstName = "Ahmed", LastName = "Dewedar", StartDate = new DateOnly(2005, 01, 01) };
 IPerson mgr = new Manager { FirstName = "mAhmed", LastName = "mDewedar", NumberOfDirectReports = 35 };
-
 Console.WriteLine(GetPersonalDetails(sw));
 Console.WriteLine(GetPersonalDetails(mgr));
 
@@ -68,16 +67,10 @@ static string GetPersonalDetails(IPerson p)
     {
     var result = p switch
         {
-            ShiftWorker swv => $"Employee : {swv.FirstName} {swv.LastName} Started: {swv.StartDate}",
+            ShiftWorker swv when swv.StartDate.Year > 2022 => $"Employee : {swv.FirstName} {swv.LastName} Started: {swv.StartDate}",
+            ShiftWorker swv when swv.StartDate.Year < 2022 => $"Employee : {swv.FirstName} {swv.LastName} Started: Older Employee.",
             Manager mgrv => $"Manager : {mgrv.FirstName} {mgrv.LastName} Reports: {mgrv.NumberOfDirectReports}",
             _ => String.Empty
             };
     return result;
-
-    ////ShiftWorker? swv = p as ShiftWorker; //as take the value and compare
-    //if(p is ShiftWorker swv) // check if p is not null as well as is shiftworker type then declare and assign it to swv
-    //    { return $"Employee : {swv.FirstName} {swv.LastName} Started: {swv.StartDate}"; }
-    //else if(p is Manager mgrv)
-    //    { return $"Manager : {mgrv.FirstName} {mgrv.LastName} Reports: {mgrv.NumberOfDirectReports}"; }
-    //return String.Empty;
     }
