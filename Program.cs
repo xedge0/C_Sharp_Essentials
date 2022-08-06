20102500fb19c0894c543ece9ea182970d3adf50
Ahmed Dewedar <dr.a.dewedar@gmail.com>
2022-08-06 11:54:49 +0200

    Test Enums

--- Program.cs
+++ Program.cs
@@ -2,12 +2,23 @@
 
 
 ShiftWorker sw = new ShiftWorker() { FirstName = "Ahmed", LastName = "Dewedar" };
-sw.DaysAvaliable = ShiftDays.Saturday | ShiftDays.Friday;
+sw.DaysAvaliable = ShiftDays.Weekdays;
 
-Console.WriteLine(sw.DaysAvaliable);
+bool avaliableMondays = sw.DaysAvaliable.HasFlag(ShiftDays.Monday);
+Console.WriteLine($"Avaliabe Mondays : {avaliableMondays}");
+
+bool avaliableFridays = (sw.DaysAvaliable & ShiftDays.Friday) == ShiftDays.Friday;
+Console.WriteLine($"Avaliable Fridays : {avaliableFridays}");
+
+//var shiftNames = Enum.GetName(typeof(ShiftDays));
+//Console.WriteLine(String.Join(',', shiftNames));
+
+var shiftDaysValue = (short[])Enum.GetValues(typeof(ShiftDays));
+Console.WriteLine(String.Join(',', shiftDaysValue));
+
 
 var day = DayOfWeek.Monday;
-Console.WriteLine(day);
+//Console.WriteLine(day);
 
 var shiftday = ShiftDays.Saturday;
-Console.WriteLine((int)shiftday);
+//Console.WriteLine((int)shiftday);
