9e605dc0428f02f7398f08552a2774e6a4159230
Ahmed Dewedar <dr.a.dewedar@gmail.com>
2022-08-08 15:13:25 +0200

    Working with collections

--- CollectionSamples.cs
+++ CollectionSamples.cs
@@ -1,17 +1,16 @@
 ﻿namespace C_Sharp_Essentials;
-
 public static class CollectionSamples
     {
     public static void Queue()
         {
-        var q = new System.Collections.Generic.Queue<string>();
+        var q = new System.Collections.Queue();
         q.Enqueue("firstItem");
         q.Enqueue("secondItem");
 
         string? item = null;
 
         Console.WriteLine("Using a Queue");
-        while((item = q.Dequeue()) != null)
+        while((item = (string?)q.Dequeue()) != null)
             {
             Console.WriteLine(item);
             if(q.Count <= 0)
@@ -34,6 +33,80 @@ public static class CollectionSamples
             Console.WriteLine(stkItem);
             if(stk.Count <= 0)
                 break;
+            }
+        }
+
+    private static List<Customer> customers;
+
+    static CollectionSamples()
+        {
+        //initialize new customers and add to list
+        customers = new List<Customer>();
+
+        for(int i = 1; i < 11; i++)
+            {
+            customers.Add(
+                new Customer
+                    {
+                    Id = i,
+                    FirstName = i.ToString(),
+                    LastName = "Customer",
+                    CreateDate = new DateOnly(2021, 10, i)
+                    });
+            }
+        }
+
+    public static void Indexing()
+        {
+        //get an item at a specific index
+        var customerThree = customers[2];
+        Console.WriteLine($"Found customer {customerThree.Id} at index 2");
+
+        //check if a collection contains an item
+        string doesContain = customers.Contains(customerThree) ? "does" : "doesn't";
+        Console.WriteLine($"Customers {doesContain} contain {customerThree?.FirstName} customer.");
+        //FOLLOW UP: What if you created a new customer object with the same property values?
+        //FOLLOW UP: Same as before, but what if customer was a record type or value type instead of a class?
+
+        //use a predicate to find an item in the collection
+        var CustomerFinder = customers.Find(CustomerIsMatch);
+
+        if(CustomerFinder != null)
+            {
+            Console.WriteLine($"Found customer {CustomerFinder.Id}");
+            }
+        else
+            {
+            Console.WriteLine($"No customer found with this ID.");
+            }
+
+        }
+
+    private static bool CustomerIsMatch(Customer c)
+        {
+        return c.Id == 17;
+        //FOLLOW UP: What happens if your expression matches more than one item?
+        }
+
+    public static void Iterating()
+        {
+        //reverse the order of the list
+        customers.Reverse();
+
+        IEnumerator<Customer> custEnum = customers.GetEnumerator();
+        while(custEnum.MoveNext())
+            {
+            Customer current = custEnum.Current;
+            Console.WriteLine($"{current.FirstName} {current.LastName}");
+            }
+
+        //sort the customers
+        customers.Sort();  // or in our case, Reverse would do the same
+
+        //or using foreach
+        foreach(var customer in customers)
+            {
+            Console.WriteLine($"{customer.FirstName} {customer.LastName}");
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -3,24 +3,5 @@
 CollectionSamples.Queue();
 CollectionSamples.Stack();
 Console.WriteLine();
-//return; // skip code
-
-string[] names = new string[2];
-names[0] = "Alex";
-names[1] = "Stone";
-
-Console.WriteLine($"Hello {names[0]}, {names[1]}");
-
-var al = new System.Collections.ArrayList(2);
-al.Add("First");
-al.Add("Second");
-al.Add("Third");
-al.AddRange(new string[] { "rSecond", "rThird", "rFourth", "rFifth" });
-
-Console.WriteLine($"Collection Size : {al.Count}");
-Console.WriteLine($"Indexed Items from the collections {al[4]}");
-Console.WriteLine("All Items from collections");
-foreach(var item in al)
-    {
-    Console.Write($"{item} ");
-    }
+CollectionSamples.Indexing();
+CollectionSamples.Iterating();
