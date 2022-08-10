a27e1d70026df4182608c2d4538a9686e6c46f16
Ahmed Dewedar <dr.a.dewedar@gmail.com>
2022-08-10 13:10:39 +0200

    Common methods on collections

--- C_Sharp_Essentials/Program.cs
+++ C_Sharp_Essentials/Program.cs
@@ -1,8 +1,19 @@
 ﻿using C_Sharp_Essential.Library;
 using C_Sharp_Essential.Library.Extensions;
 
-var right = C_Sharp_Essential.Library.Extensions.StringExtensions.Right("Hello Four", 4);
-Console.WriteLine(right);
+//var right = C_Sharp_Essential.Library.Extensions.StringExtensions.Right("Hello Four", 4);
+//Console.WriteLine(right);
 
-var s = "right fives";
-Console.WriteLine(s.Right(5));
+//var s = "right fives";
+//Console.WriteLine(s.Right(5));
+
+var names = new List<string> { "Edge", "Stone", "Alex", "Wall", "Xi" };
+
+var shortest = names.MinBy((s) => s.Length);
+Console.WriteLine($"Shortest Name : {shortest}");
+
+var orderedNames = names.OrderBy(s => s.Length);
+foreach (var name in orderedNames)
+    {
+    Console.WriteLine(name);
+    }
