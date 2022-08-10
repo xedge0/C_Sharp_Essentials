d30c842df625e4f7811700b68d9fe40a249159f6
Ahmed Dewedar <dr.a.dewedar@gmail.com>
2022-08-10 13:38:48 +0200

    LINQ Query Collections

--- C_Sharp_Essentials/Program.cs
+++ C_Sharp_Essentials/Program.cs
@@ -14,8 +14,12 @@ var filteredEmployees = employees
         .Where((e) => e.ID >= 2)
         .Select((es) => new { FirstName = es.FirstName, LastName = es.LastName });
 
+var fEmployee = from emp in employees
+                where emp.ID >= 2
+                select new { FirstName = emp.FirstName, LastName = emp.LastName };
 
-foreach(var e in filteredEmployees)
+
+foreach(var emp in fEmployee)
     {
-    Console.WriteLine(e.FirstName);
+    Console.WriteLine(emp.FirstName);
     }
