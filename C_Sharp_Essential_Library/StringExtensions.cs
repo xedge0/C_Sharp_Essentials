c10d9f38f8d9ead79188a21280474e7dc4267028
Ahmed Dewedar <dr.a.dewedar@gmail.com>
2022-08-10 12:51:42 +0200

    Writing extension method

--- C_Sharp_Essential_Library/StringExtensions.cs
+++ C_Sharp_Essential_Library/StringExtensions.cs
@@ -8,7 +8,7 @@ namespace C_Sharp_Essential.Library.Extensions;
 
 public static class StringExtensions
     {
-    public static string Right(string input, int numChars)
+    public static string Right(this string input, int numChars)
         {
         if(string.IsNullOrEmpty(input))
             { return string.Empty; }
