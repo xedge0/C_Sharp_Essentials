9b515fcd99fa64b9ead195a4eab70cbf412fdcc5
Ahmed Dewedar <dr.a.dewedar@gmail.com>
2022-08-10 11:28:47 +0200

    Events basics

--- C_Sharp_Essential_Library/DelegateSamples.cs
+++ C_Sharp_Essential_Library/DelegateSamples.cs
@@ -2,10 +2,19 @@
 
 public static class DelegateSamples
     {
+    public static event Del SomethingHappened;
     public delegate void Del(string input);
 
     public static void PassMeWork(Del work)
         {
         work("Delegates");
+        }
+    public static void DoSomething()
+        {
+        Console.WriteLine("I'am about to do something.");
+        if (SomethingHappened != null)
+            {
+            SomethingHappened("I did something");
+            }
         }
     }
--- C_Sharp_Essentials/Program.cs
+++ C_Sharp_Essentials/Program.cs
@@ -1,5 +1,8 @@
 ﻿using C_Sharp_Essential_Library;
 
+Console.WriteLine("Hit Ctrl + C to cancel");
+Console.CancelKeyPress += OnCancel;
+
 Delegate del = WritingHello;
 del.DynamicInvoke("Ahmed");
 DelegateSamples.PassMeWork(WritingHello);
@@ -7,5 +10,19 @@ DelegateSamples.PassMeWork(WritingHello);
 static void WritingHello(string name)
     {
     Console.WriteLine($"Hello, {name}");
+    DelegateSamples.SomethingHappened -= WritingHello; 
     }
 
+DelegateSamples.SomethingHappened += WritingHello;
+DelegateSamples.DoSomething();
+
+for(int i = 0; i < 100000; i++)
+    {
+    Console.WriteLine(i);
+    }
+
+static void OnCancel(object sender, ConsoleCancelEventArgs args)
+    {
+    args.Cancel = false; //true
+    Console.CancelKeyPress -= OnCancel;
+    }
