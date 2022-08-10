d9899981d041b63c9fd0c4540c758bcd99b14a28
Ahmed Dewedar <dr.a.dewedar@gmail.com>
2022-08-10 13:04:38 +0200

    using extension method

--- C_Sharp_Essentials/Program.cs
+++ C_Sharp_Essentials/Program.cs
@@ -1,7 +1,8 @@
 ﻿using C_Sharp_Essential.Library;
+using C_Sharp_Essential.Library.Extensions;
 
 var right = C_Sharp_Essential.Library.Extensions.StringExtensions.Right("Hello Four", 4);
 Console.WriteLine(right);
 
-var s = "right four";
-//s.right();
+var s = "right fives";
+Console.WriteLine(s.Right(5));
